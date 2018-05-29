e6463c9bdcb2058d47603255a6af1db6caed712e
Denys Kulinich <>
2018-05-29 14:51:47 +0300

    test test

--- test/repeat.js
+++ test/repeat.js
@@ -1,7 +1,8 @@
 var assert = require('assert');
 var sinon = require("sinon");
 repeat = require('../solution/repeat.js');
-
+// test
+// test
 describe('repeat()', function(){
     it('should repeat function', function(){
         var spy = sinon.spy();
