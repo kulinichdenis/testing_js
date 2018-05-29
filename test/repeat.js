85ac359ba3cd84a54020b640db3cb59fab9e0047
Denys Kulinich <>
2018-05-29 14:50:57 +0300

    test1

--- test/repeat.js
+++ test/repeat.js
@@ -5,8 +5,10 @@ repeat = require('../solution/repeat.js');
 describe('repeat()', function(){
     it('should repeat function', function(){
         var spy = sinon.spy();
+
+
         repeat(spy, 5);
-        // test
+        //
         assert.equal(spy.callCount,5);
     });
 });
